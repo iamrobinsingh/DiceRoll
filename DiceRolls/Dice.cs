e2dc7c54b26da0ea9677a23ff58d5b714692aa92
iamrobinsingh <singhrobin0901@gmail.com>
2021-03-08 20:10:31 +0530

    Add project files.

---
+++ DiceRolls/Dice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRolls
+{
+    public static class Dice
+    {
+        public static int RollOutcome()
+        {
+            Random _random = new Random();
+            return _random.Next(1, 7);
+        }
+    }
+}
---
+++ DiceRolls/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DiceRolls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            sum += Dice.RollOutcome();
+            sum += Dice.RollOutcome();
+            sum += Dice.RollOutcome();
+            sum += Dice.RollOutcome();
+            sum += Dice.RollOutcome();
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+
+            /*
+            //Another Approach             
+            Random _random = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                sum += _random.Next(1, 7);
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            */
+
+        }
+    }
+}
